c7b9b53099b4254161254a927846ec92a2670a0b
Makson <144148309+Sky65678@users.noreply.github.com>
2024-09-17 00:50:30 +0300

    rework style btn

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -7,15 +7,17 @@ export default function SearchBar({ onSubmit }) {
   return (
     <Formik initialValues={{ image: "" }} onSubmit={onSubmit}>
       <Form className={css.header}>
-        <button className={css.inputButton} type="submit">
-          <IoSearchOutline size="22px" />
-        </button>
-        <Field
-          className={css.input}
-          type="text"
-          name="image"
-          placeholder="Search images and photos"
-        />
+        <div className={css.btnInput}>
+          <button className={css.inputButton} type="submit">
+            <IoSearchOutline size="22px" />
+          </button>
+          <Field
+            className={css.input}
+            type="text"
+            name="image"
+            placeholder="Search images and photos"
+          />
+        </div>
       </Form>
     </Formik>
   );
